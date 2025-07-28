b07e596bf9c50b4ad03900533a9b7fe33ae09cf1
Tanaphat Partoom <76895893+ShadowsDuck@users.noreply.github.com>
2025-07-29 01:14:02 +0700

    cron.js added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,5 @@
 import express from "express";
-// import job from "./config/cron.js";
+import job from "./config/cron.js";
 import { clerkMiddleware } from "@clerk/express";
 import userRoutes from "./routes/user.route.js";
 import classRoutes from "./routes/class.route.js";
@@ -10,7 +10,7 @@ import roomRoutes from "./routes/rooms.route.js";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// if (process.env.NODE_ENV === "production") job.start();
+if (process.env.NODE_ENV === "production") job.start();
 
 // middleware
 app.use(clerkMiddleware());
