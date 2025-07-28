02d298d93fdc2e8cb73f2b9289b58484eff86ded
Tanaphat Partoom <76895893+ShadowsDuck@users.noreply.github.com>
2025-07-28 22:04:17 +0700

    statusbar light when loading

--- mobile/components/Loading.jsx
+++ mobile/components/Loading.jsx
@@ -1,9 +1,11 @@
+import { StatusBar } from "expo-status-bar";
 import { View, ActivityIndicator } from "react-native";
 
 const Loading = ({ size = "large", color = "white" }) => {
   return (
     <View className="flex-1 justify-center items-center bg-[#121212]">
       <ActivityIndicator size={size} color={color} />
+      <StatusBar style="light" />
     </View>
   );
 };
